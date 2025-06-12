9fd3651a49b65434cfa43272a7b4a5fb05b0a707
rene.reyes <rene.reyes@bcr.gob.sv>
2025-06-12 04:12:20 -0600

    eliminacion de proceso rollout app.py

--- microservice/hello-world/app.py
+++ microservice/hello-world/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from GitHub CI/CD 🛠️🚀, esta es otra prueba casi terminando a las 4 am'
+    return 'Hello World from GitHub CI/CD 🛠️🚀, esta es otra prueba casi terminando a las 4 am, prueba de eliminacion rollout'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5678)
