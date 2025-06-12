cc399e854069d51add627ff3df535671390fa4d2
rene.reyes <rene.reyes@bcr.gob.sv>
2025-06-12 03:32:48 -0600

    prueba de flujo

--- microservice/hello-world/app.py
+++ microservice/hello-world/app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from GitHub CI/CD 🛠️🚀'
+    return 'Hello World from GitHub CI/CD 🛠️🚀, te amo mucho Yesenia'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5678)
